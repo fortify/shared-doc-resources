c25aa9d96c55be5b2d3118697f349fe14836dfc4
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-01-27 11:32:23 +0100

    Improve & enhance update-doc-resources.sh

--- scripts/update-doc-resources.sh
+++ scripts/update-doc-resources.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-SHARED_TEMPLATES_DIR=https://raw.githubusercontent.com/fortify/shared-doc-resources/main/templates
-#SHARED_TEMPLATES_DIR=file://${SCRIPT_DIR}/../templates
 DOC_RESOURCES_DIR=${SCRIPT_DIR}
 DOC_TARGET_DIR=${DOC_RESOURCES_DIR}/..
 VALUES_FILE=${DOC_RESOURCES_DIR}/template-values.md
+# Define template locations:
+# - Local templates dir in doc-resources parent directory,
+#   to allow for local testing of this script in the shared-doc-resources repo
+# - Local templates dir in doc-resources directory, 
+#   allowing repo's to override shared templates (not recommended) or to add templates once we add support for specifying extra templates
+# - Remote templates dir in shared-doc-resources repo,
+#   which will be the default behavior for most repo's     
+TEMPLATE_LOCATIONS=("file://${DOC_RESOURCES_DIR}/../templates" "file://${DOC_RESOURCES_DIR}/templates" "https://raw.githubusercontent.com/fortify/shared-doc-resources/main/templates")
+# Define include locations:
+# - Local doc-resources dir
+# - Remote includes dir in shared-doc-resources repo
+INCLUDE_LOCATIONS=("file://${DOC_RESOURCES_DIR}" "https://raw.githubusercontent.com/fortify/shared-doc-resources/main/includes")
 
 declare -A TEMPLATE_TO_TARGET_MAP=( 
   ["CODE_OF_CONDUCT.template.md"]="CODE_OF_CONDUCT.md" 
@@ -17,72 +27,120 @@ declare -A BUILT_IN_VARS=(
   ["year"]="$(date +"%Y")" 
 )
 
-ERRORS=()
+ERRORS=0
 
-addError() {
-  ERRORS[${#ERRORS[@]}]="$@"
+logError() {
+	echo "ERROR: $@" >&2
+	ERRORS=1
+}
+
+logInfo() {
+	echo "INFO: $@" >&2
 }
 
 expandVar() {
-	val=${BUILT_IN_VARS[$var]}
+	local var="$1"
+	local templateLine="$2"
+	local val=${BUILT_IN_VARS[$var]}
 	if [ -z "$val" ]; then
       # Read variable value by taking all text between '# <var>' and the next occurance of '# '
       IFS= val=$(sed -n "/^# ${var}\s*/,/^# /{//!p;}" ${VALUES_FILE})
     fi
     if [ -z "$val" ]; then
-      addError "ERROR: Value for variable '${var}' not found in ${VALUES_FILE}"
+      logError "Value for variable '${var}' not found in ${VALUES_FILE}"
     else 
       # Use bash variable expansion to replace the variable reference with the variable value
       echo "${templateLine/\{\{var:${var}\}\}/$val}"
     fi
 }
 
-expandInclude() {
-    local resolvedInclude=${DOC_RESOURCES_DIR}/${include}
-	if [[ ! -f "$resolvedInclude" ]]; then
-	  addError "ERROR: Include file $resolvedInclude (referenced as $include in ${TEMPLATE_FILE}) not found"
-	else
-	  cat "${resolvedInclude}"
-	fi
-}
-
 expandTemplateLine() {
-	var=$(echo $templateLine | sed -n 's/.*{{var:\([a-z_]*\)}}.*/\1/p')
-    include=$(echo $templateLine | sed -n 's/.*{{include:\([^}]*\)}}.*/\1/p')
+	local templateLine="$1"
+	local var=$(echo $templateLine | sed -n 's/.*{{var:\([a-z_]*\)}}.*/\1/p')
+    local include=$(echo $templateLine | sed -n 's/.*{{include:\([^}]*\)}}.*/\1/p')
     if [ -n "$var" ]; then
-      expandVar
+      expandVar "$var" "$templateLine"
     elif [ -n "$include" ]; then
-      expandInclude
+      expandIncludeResource "$include"
     else
       # Line doesn't contain variable or include, so output as-is
       echo "$templateLine"
     fi 
 }
 
-expandVars() {
+expandLines() {
 	# Read the template file line by line, clearing IFS to keep leading whitespace
 	while IFS= read -r templateLine; do  
-	    expandTemplateLine
+	    expandTemplateLine "$templateLine"
 	done
-	# If last line in template didn't end with a newline, we need to call expandTemplateLine one last time 
-	if [ -n "$templateLine" ]; then
-		expandTemplateLine
+}
+
+getResource() {
+    # Get contents and add newline at end to allow for proper reading with 'read'
+	curl -sfL "$1" && echo;
+}
+
+getFirstResource() {
+	local file="$1"; shift;
+	local locations=("$@")
+	for location in "${locations[@]}"; do
+      if getResource "$location/$file"; then
+        logInfo "Processing $location/$file"
+        return 0;
+      fi
+    done
+    logError "File $file not found in any of the following locations: ${locations[@]}"
+    return 1;
+}
+
+getTemplateResource() {
+	getFirstResource "$1" "${TEMPLATE_LOCATIONS[@]}"
+}
+
+expandTemplateResource() {
+	local output
+	if ! IFS= output=$(getTemplateResource "$1"); then
+    	ERRORS=1
+    else
+    	expandLines <<< "${output}"
+    fi
+}
+
+getIncludeResource() {
+	getFirstResource "$1" "${INCLUDE_LOCATIONS[@]}"
+}
+
+expandIncludeResource() {
+	local output
+	if ! IFS= output=$(getIncludeResource "$1"); then
+    	ERRORS=1
+    else
+    	expandLines <<< "${output}"
+    fi
+}
+
+expandTemplates() {
+	for TEMPLATE_FILE in ${!TEMPLATE_TO_TARGET_MAP[@]}; do
+		if [ "$dryRun" = "1" ]; then
+			expandTemplateResource "$TEMPLATE_FILE" > /dev/null
+		else
+			local target="${DOC_TARGET_DIR}/${TEMPLATE_TO_TARGET_MAP[$TEMPLATE_FILE]}"
+			expandTemplateResource "$TEMPLATE_FILE" > $target
+			logInfo "Generated $target"	
+		fi
+	done	
+}
+
+checkErrors() {
+	if [ "$ERRORS" = "1" ]; then
+	    echo "Errors encountered; document resources will not be updated" >&2
+		return 1
 	fi
 }
 
-for TEMPLATE_FILE in ${!TEMPLATE_TO_TARGET_MAP[@]}; do
-    # TODO Generate error if template file cannot be found or is empty
-	expandVars < <(curl -sL "${SHARED_TEMPLATES_DIR}/$TEMPLATE_FILE"; echo;) > /dev/null
-done
+logInfo "=== Performing dry-run to check for errors ==="
+dryRun=1 expandTemplates
+checkErrors && logInfo "=== Generating documentation resources ===" && expandTemplates && logInfo "=== Done ==="
 
-if [ -n "$ERRORS" ]; then
-    echo "Errors encountered; document resources will not be updated" >&2
-	for err in "${ERRORS[@]}"; do
-      echo "$err"
-	done
-	exit 1
-fi
 
-for TEMPLATE_FILE in ${!TEMPLATE_TO_TARGET_MAP[@]}; do
-	expandVars < <(curl -sL "${SHARED_TEMPLATES_DIR}/$TEMPLATE_FILE") > "${DOC_TARGET_DIR}/${TEMPLATE_TO_TARGET_MAP[$TEMPLATE_FILE]}"
-done
+
