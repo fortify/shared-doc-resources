1b7a833b792d9f9e8597268fa03fc91202719a23
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-01-30 13:56:55 +0100

    Restructure & add setup resources

--- setup/setup-github.sh
+++ setup/setup-github.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 EXTRA_RESOURCES=(".github/workflows/update-repo-docs.yml") 
-source source <(curl -s https://raw.githubusercontent.com/fortify/shared-doc-resources/main/setup/setup.sh)
+source <(curl -s https://raw.githubusercontent.com/fortify/shared-doc-resources/main/setup/setup.sh)
